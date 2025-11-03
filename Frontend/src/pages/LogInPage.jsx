a6c5d0a457f512aa2f645354f6164f6bed630d0a
n0thing2c <trannguyenduythinhhvn@gmail.com>
2025-11-03 22:22:51 +0700

    add login-form.jsx + LogInPage.jsx

---
+++ Frontend/src/pages/LogInPage.jsx
@@ -0,0 +1,19 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { LoginForm } from "@/components/ui/login-form";
+
+const LogInPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <Header />
+      <main className="grow flex flex-col items-center px-6 md:px-10 pt-2 md:pt-4 pb-8 md:pb-12">
+        <div className="w-full max-w-sm md:max-w-4xl">
+          <LoginForm />
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default LogInPage;
