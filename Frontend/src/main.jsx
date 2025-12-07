ecc5b5309a8a621461ff3c253c5cf69ac6494282
YuukiT <truongtandung1511@gmail.com>
2025-12-07 15:54:53 +0700

    Change title and logo

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from "./App.jsx";
 import "./index.css"
+import logo from "./assets/LogoVNGO.png"
+
+// Set favicon from assets
+const setFavicon = (iconPath) => {
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/png';
+  link.rel = 'icon';
+  link.href = iconPath;
+  document.getElementsByTagName('head')[0].appendChild(link);
+  
+  // Set apple-touch-icon for mobile
+  const appleLink = document.querySelector("link[rel*='apple-touch-icon']") || document.createElement('link');
+  appleLink.rel = 'apple-touch-icon';
+  appleLink.href = iconPath;
+  document.getElementsByTagName('head')[0].appendChild(appleLink);
+};
+
+setFavicon(logo);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
